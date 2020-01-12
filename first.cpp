1d7d2d29647d963f0b4bd275dd92c6b3e6754865
Aayush <aayush.somani111@gmail.com>
2020-01-12 11:07:54 +0530

    First commit

---
+++ first.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class game
+{
+    private:
+        bool quit;
+    public:
+        
+        void Draw()
+        {
+
+        }
+
+        void Input()
+        {
+
+        }
+        
+        void Logic()
+        {
+
+        }
+
+        void Run()
+        {
+            while(!quit)
+            {
+            Draw();
+            Input();
+            Logic();
+            }
+        }
+
+};
+
