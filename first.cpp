21bc46521bc31e36023ce9775e2eff854599a686
Aayush <aayush.somani111@gmail.com>
2020-01-12 11:20:24 +0530

    lane class made

--- first.cpp
+++ first.cpp
@@ -1,6 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class player
+{
+    private:
+        int x,y; //x and y coordinates of the player
+    public:
+        player(int width) //initialised at the center
+        {
+            x=width/2;
+            y=0;
+        }
+};
+
+class Lane
+{
+    private:
+        deque<bool> cars;
+        int width_;   //will contain the set of cars in that lane
+    public:
+        Lane(int width)     //initialised with all false, i.e. no cars
+        {
+            width_=width;
+            for(int i=0;i<width_;i++)
+            {
+                cars.push_front(false);
+            }
+        }
+
+        void move()
+        {
+            if(rand()%6==0)
+            {
+                cars.push_front(true);
+            } else cars.push_front(false);
+
+            cars.pop_back();
+        }
+
+        bool checkPos(int pos)
+        {
+            if (pos<width_)
+                return cars[pos];
+            else return false;
+        }
+};
 class game
 {
     private:
@@ -26,8 +70,8 @@ class game
         {
             while(!quit)
             {
-            Draw();
             Input();
+            Draw();
             Logic();
             }
         }
